694857573cee5cf463b1e0edbe3cc696b0e2f009
Zach Gray <zgray@avantia-inc.com>
2016-02-29 17:45:32 -0500

    commenting, cleanup

--- src/main/kotlin/rxg/dsl/dsl.kt
+++ src/main/kotlin/rxg/dsl/dsl.kt
@@ -1,8 +1,10 @@
 package rxg.dsl
 
+/**
+ * Most of the gameplay DSL is defined in GameplayEngine interface out of necessity, but since values cant be defined
+ * in an interface, some helpers are defined here as well.
+ */
 object GameplayDSL {
-   // companion object {
-        val ifNot:(notValue:Boolean, closure:()->Unit) -> ()->Unit = { notValue, closure -> if(!notValue) closure else { {} } }
-        val ifIs:(isValue:Boolean, closure:()->Unit) -> ()->Unit = { isValue, closure -> if(isValue) closure else { {} } }
-    //}
+    val ifNot:(notValue:Boolean, closure:()->Unit) -> ()->Unit = { notValue, closure -> if(!notValue) closure else { {} } }
+    val ifIs:(isValue:Boolean, closure:()->Unit) -> ()->Unit = { isValue, closure -> if(isValue) closure else { {} } }
 }
